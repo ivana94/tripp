3003b0b60a1465789693909c04b7050720186b7f
Ivana Matijevic <ivmatij@gmail.com>
2019-06-18 09:16:03 +0200

    tables, they be a changin

--- sql/expenses.sql
+++ sql/expenses.sql
@@ -2,11 +2,15 @@ DROP TABLE IF EXISTS expenses;
 
 CREATE TABLE expenses (
     id SERIAL PRIMARY KEY,
-    expense TEXT NOT NULL,
-    city INT,
+    item VARCHAR(255) NOT NULL,
+    is_activity BOOLEAN DEFAULT FALSE,
+    is_expense BOOLEAN DEFAULT FALSE,
+    price INT,
+    city VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO expenses (expense) VALUES ('flight');
-INSERT INTO expenses (expense) VALUES ('lodging');
-INSERT INTO expenses (expense) VALUES ('JR Pass');
+INSERT INTO expenses (item, is_activity, city, price) VALUES ('Ghibli Museum', true, 'Tokyo', 9);
+INSERT INTO expenses (item, is_expense, price) VALUES ('flight', true, 1000);
+INSERT INTO expenses (item, is_expense, price) VALUES ('lodging', true, 1000);
+INSERT INTO expenses (item, is_expense, price) VALUES ('JR Pass', true, 414);
