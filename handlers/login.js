0466323d9b0d57758ee417bc7e8b37e31fc3739e
Ivana Matijevic <ivmatij@gmail.com>
2019-07-14 13:56:18 +0200

    updated form validation

--- handlers/login.js
+++ handlers/login.js
@@ -3,9 +3,9 @@ const auth = require('../lib/auth');
 
 const handleLogin = async (req, res, next) => {
     const user = await users.getUserByEmail(req.body.email);
-    if (!user) throw new Error('incorrect email');
+    if (!user) throw new Error('try again');    
     const doesMatch = await auth.compare(req.body.password, user.password);
-    if (!doesMatch) throw new Error(`jesus christ ${user.first}, did you forget your password?`);
+    if (!doesMatch) throw new Error('try again');
     req.session.userId = user.id;
     res.json({ success: true });
 };
